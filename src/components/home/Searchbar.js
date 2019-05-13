036941cd4a96484ad795806858ac055b0e444077
Ronyonka <rononyonka@gmail.com>
2019-05-13 07:07:44 +0300

    added repo component

--- src/components/home/Searchbar.js
+++ src/components/home/Searchbar.js
@@ -1,71 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
 
-import * as userActions from '../../actions/userAction';
-import Repo from './Repo';
-import Searchbar from './Searchbar';
-
-class HomePage extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			user: ""
-		};
-		this.updateSearch = this.updateSearch.bind(this);
-		this.saveSearch = this.saveSearch.bind(this);
-	}
-
-	updateSearch(e) {
-		let user = e.target.value;
-		return this.setState({ user: user });
-	}
-
-	saveSearch(e) {
-		e.preventDefault();
-		this.props.actions.loadUser(this.state.user);
-	}
-
-	repoRow(repo, index) {
-		return (
-			<div key={index}>
-				<Repo key={repo.id} repo={repo} />
-			</div>
-		);
-	}
-
-	render() {
-		return (
-			<div className="container">
-				<Searchbar
-					user={this.state.user}
-					onChange={this.updateSearch}
-					onSave={this.saveSearch}
-				/>
-				{this.props.repos.map(this.repoRow)}
-			</div>
-		);
-	}
-}
-
-HomePage.propTypes = {
-	repos: PropTypes.array.isRequired,
-	user: PropTypes.string.isRequired,
-	actions: PropTypes.object.isRequired
+const Searchbar = ({ user, onChange, onSave }) => {
+	return (
+		<div className="container">
+			<form onSubmit={onSave}>
+				<div className="input-field">
+					<i className="fa fa-search material-icons prefix red-text text-darken-4" aria-hidden="true" />
+					<input id="icon_prefix" type="text" className="validate" placeholder="Github username" value={user} onChange={onChange} />
+				</div>
+			</form>
+		</div>
+	);
 };
 
-function mapStateToProps(state) {
-	return {
-		repos: state.repos,
-		user: state.user
-	};
-}
+Searchbar.propTypes = {
+	user: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+	onSave: PropTypes.func.isRequired
+};
 
-function mapDispatchToProps(dispatch) {
-	return {
-		actions: bindActionCreators(userActions, dispatch),
-	};
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+export default Searchbar;
