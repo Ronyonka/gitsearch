0ad5681414e2e85979ba9456bc48c1f2a53baa45
Ronyonka <rononyonka@gmail.com>
2019-05-13 08:23:04 +0300

    debugged some changes

--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { Router } from 'react-router';
-import { createBrowserHistory } from 'history'
-import 'materialize-css/dist/css/materialize.min.css';
-import { Provider } from 'react-redux';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-import routes from './routes';
-import configureStore from './store/configureStore';
-import { loadRepos } from './actions/reposAction';
-
-let currentValue;
-function handleChange() {
-	let previousValue = currentValue;
-	currentValue = store.getState().user;
-
-	if (previousValue !== currentValue) {
-		store.dispatch(loadRepos(store.getState().user));
-	}
-}
-
-const store = configureStore();
-store.dispatch(loadRepos(store.getState().user));
-store.subscribe(handleChange);
-
-render(
-	<Provider store={store}>
-		<Router history={createBrowserHistory} routes={routes} />
-	</Provider>,
-	document.getElementById('app')
-);
+ReactDOM.render(<App />, document.getElementById('root'));
