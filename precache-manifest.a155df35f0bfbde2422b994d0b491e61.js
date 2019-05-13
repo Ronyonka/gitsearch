69db7c619a3b5e65ec8f7305d8c5eaf7c89e3195
Ronyonka <rononyonka@gmail.com>
2019-05-13 15:34:08 +0300

    Updates

---
+++ precache-manifest.a155df35f0bfbde2422b994d0b491e61.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89bf8cb6323ca7eef07b780e0d292f4",
+    "url": "/gitsearch/index.html"
+  },
+  {
+    "revision": "0843a8e7e3bc6348a3fb",
+    "url": "/gitsearch/static/js/2.b3ecf57a.chunk.js"
+  },
+  {
+    "revision": "0485956b967aaa489a4e",
+    "url": "/gitsearch/static/js/main.72fd9a9e.chunk.js"
+  },
+  {
+    "revision": "293a3cadf652db67f739",
+    "url": "/gitsearch/static/js/runtime~main.810e9d08.js"
+  }
+]);
